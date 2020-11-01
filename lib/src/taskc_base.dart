be6a69c88e243696e07202bd329658c2f0cd5146
Brady Trainor <git_commit@tangential.info>
2020-11-01 00:00:00 +0000

    Add taskserver client socket code

--- lib/src/taskc_base.dart
+++ lib/src/taskc_base.dart
@@ -1,6 +1,66 @@
-// TODO: Put public facing types in this file.
+import 'dart:io';
+import 'dart:typed_data';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class TaskdConnection {
+  TaskdConnection({
+    this.clientCert,
+    this.clientKey,
+    this.cacertFile,
+    this.server,
+    this.port,
+    this.group,
+    this.username,
+    this.uuid,
+  });
+
+  final String clientCert;
+  final String clientKey;
+  final String cacertFile;
+  final String server;
+  final int port;
+  final String group;
+  final String username;
+  final String uuid;
+
+  factory TaskdConnection.fromTaskrc(String taskrc) {
+    File file = File(taskrc);
+    var xs = file.readAsStringSync().split('\n');
+    var conf = Map.fromIterable(
+      xs
+          .where((x) => x.contains('=') && x[0] != '#')
+          .map((x) => x.replaceAll('\\/', '/'))
+          .map((x) => x.split('=')),
+      key: (item) => item[0],
+      value: (item) => item[1],
+    );
+
+    return TaskdConnection(
+      clientCert: conf['taskd.certificate'],
+      clientKey: conf['taskd.key'],
+      cacertFile: conf['taskd.ca'],
+      server: conf['taskd.server'].split(':').first,
+      port: int.parse(conf['taskd.server'].split(':').last),
+      group: conf['taskd.credentials'].split('/').first,
+      username: conf['taskd.credentials'].split('/')[1],
+      uuid: conf['taskd.credentials'].split('/').last,
+    );
+  }
+
+  Future<Uint8List> sendMessageAsBytes(Uint8List message) async {
+    SecurityContext context = SecurityContext()
+      ..useCertificateChain(clientCert)
+      ..usePrivateKey(clientKey)
+      ..setTrustedCertificates(cacertFile);
+
+    SecureSocket socket = await SecureSocket.connect(
+      server,
+      port,
+      context: context,
+      onBadCertificate: (_) => true,
+    );
+
+    socket.add(message);
+
+    return socket.first;
+  }
 }
--- test/taskc_test.dart
+++ test/taskc_test.dart
@@ -1,16 +1,72 @@
-import 'package:taskc/taskc.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:test/test.dart';
 
+import 'package:taskc/taskc.dart';
+
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+  group('Test rc parse', () {
+    var tc;
+    var home;
 
     setUp(() {
-      awesome = Awesome();
+      home = Platform.environment['HOME'];
+      tc = TaskdConnection.fromTaskrc('$home/.taskrc');
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(tc.clientCert, '$home/.task/brady_trainor.cert.pem');
+      expect(tc.clientKey, '$home/.task/brady_trainor.key.pem');
+      expect(tc.cacertFile, '$home/.task/ca.cert.pem');
+      expect(tc.server, 'localhost');
+      expect(tc.port, 53589);
+      expect(tc.group, 'Public');
+      expect(tc.username, 'Brady Trainor');
+      expect(tc.uuid, '69eeece5-bcda-4ba2-a34c-70fdbbbe6187');
+    });
+  });
+  group('Test socket', () {
+    var tc;
+    setUp(() {
+      var home = Platform.environment['HOME'];
+      tc = TaskdConnection(
+        clientCert: '$home/.task/brady_trainor.cert.pem',
+        clientKey: '$home/.task/brady_trainor.key.pem',
+        cacertFile: '$home/.task/ca.cert.pem',
+        server: 'localhost',
+        port: 53589,
+        group: 'Public',
+        username: 'Brady Trainor',
+        uuid: '69eeece5-bcda-4ba2-a34c-70fdbbbe6187',
+      );
+    });
+
+    test('test socket', () async {
+      var response =
+          await tc.sendMessageAsBytes(Uint8List.fromList([0, 0, 0, 5, 65]));
+
+      expect(response.length, 69);
+    });
+  });
+  group('Test message encoding/decoding', () {
+    var messageBytes;
+    var messageString;
+    setUp(() {
+      messageBytes = Uint8List.fromList(List.from(json.decode(
+          '[0, 0, 0, 69, 99, 108, 105, 101, 110, 116, 58, 32, 116, 97, 115, 107, 100, 32, 49, 46, 49, 46, 48, 10, 99, 111, 100, 101, 58, 32, 53, 48, 48, 10, 115, 116, 97, 116, 117, 115, 58, 32, 69, 82, 82, 79, 82, 58, 32, 77, 97, 108, 102, 111, 114, 109, 101, 100, 32, 109, 101, 115, 115, 97, 103, 101, 10, 10, 10]')));
+      messageString = '''
+client: taskd 1.1.0
+code: 500
+status: ERROR: Malformed message
+
+
+''';
+    });
+
+    test('test decode', () async {
+      expect(utf8.decode(messageBytes.skip(4).toList()), messageString);
     });
   });
 }
