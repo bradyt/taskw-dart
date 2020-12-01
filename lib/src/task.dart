df840776181e213d0b0b830498e40c92f639fd28
Brady Trainor <git_commit@tangential.info>
2020-12-01 00:00:00 +0000

    Add Annotation and DateTime formats to Task type

--- lib/src/task.dart
+++ lib/src/task.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 
-DateFormat format = DateFormat('yMMddTHHmmss\'Z\'');
+DateFormat iso8601Basic = DateFormat('yMMddTHHmmss\'Z\'');
 
 @immutable
 class Task {
@@ -25,7 +25,7 @@ class Task {
     this.priority,
     this.depends,
     this.tags,
-    this.annotation,
+    this.annotations,
   });
 
   factory Task.fromJson(Map json) => Task(
@@ -46,13 +46,15 @@ class Task {
             : DateTime.parse(json['scheduled']),
         recur: json['recur'],
         mask: json['mask'],
-        imask: json['imask'],
+        imask: json['imask']?.toInt(),
         parent: json['parent'],
         project: json['project'],
         priority: json['priority'],
         depends: json['depends'],
         tags: json['tags'],
-        annotation: json['annotation'],
+        annotations: json['annotations']
+            ?.map<Annotation>((annotation) => Annotation.fromJson(annotation))
+            ?.toList(),
       );
 
   final String status;
@@ -74,12 +76,12 @@ class Task {
   final String priority;
   final String depends;
   final String tags;
-  final String annotation;
+  final List<Annotation> annotations;
 
   Map toJson() => {
         'status': status,
         'uuid': uuid,
-        'entry': format.format(entry),
+        'entry': entry,
         'description': description,
         'start': start,
         'end': end,
@@ -91,13 +93,19 @@ class Task {
         'mask': mask,
         'imask': imask,
         'parent': parent,
-        'annotation': annotation,
+        'annotations':
+            annotations?.map((annotation) => annotation.toJson())?.toList(),
         'project': project,
         'tags': tags,
         'priority': priority,
         'depends': depends,
         'modified': modified,
-      }..removeWhere((_, value) => value == null);
+      }
+        ..removeWhere((_, value) => value == null)
+        ..updateAll((key, value) =>
+            'entry,start,end,due,until,scheduled,wait,modified'.contains(key)
+                ? iso8601Basic.format(value)
+                : value);
 
   @override
   int get hashCode => 0;
@@ -119,10 +127,48 @@ class Task {
       mask == other.mask &&
       imask == other.imask &&
       parent == other.parent &&
-      annotation == other.annotation &&
+      _listEquals(annotations, other.annotations) &&
       project == other.project &&
       tags == other.tags &&
       priority == other.priority &&
       depends == other.depends &&
       modified == other.modified;
+
+  // copied from 'package:flutter/foundation.dart'
+  bool _listEquals<T>(List<T> a, List<T> b) {
+    if (a == null) return b == null;
+    if (b == null || a.length != b.length) return false;
+    if (identical(a, b)) return true;
+    for (var index = 0; index < a.length; index += 1) {
+      if (a[index] != b[index]) return false;
+    }
+    return true;
+  }
+}
+
+@immutable
+class Annotation {
+  const Annotation({this.entry, this.description});
+
+  factory Annotation.fromJson(Map annotation) => Annotation(
+        entry: DateTime.parse(annotation['entry']),
+        description: annotation['description'],
+      );
+
+  final DateTime entry;
+  final String description;
+
+  Map toJson() => {
+        'entry': iso8601Basic.format(entry),
+        'description': description,
+      };
+
+  @override
+  int get hashCode => 0;
+
+  @override
+  bool operator ==(Object other) =>
+      other is Annotation &&
+      entry == other.entry &&
+      description == other.description;
 }
--- test/task_test.dart
+++ test/task_test.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
@@ -7,15 +8,55 @@ import 'package:taskc/taskc.dart';
 
 void main() {
   group('Test task objects', () {
-    var task = Task(
-      status: 'pending',
-      uuid: Uuid().v1(),
-      entry: DateTime.fromMillisecondsSinceEpoch(0, isUtc: true),
-      description: 'test',
-    );
+    var unixEpoch = DateTime.fromMillisecondsSinceEpoch(0, isUtc: true);
 
-    test('test parsing json task', () {
-      expect(Task.fromJson(json.decode(json.encode(task.toJson()))), task);
+    test('test parsing simple json task', () {
+      var simpleTask = Task(
+        status: 'pending',
+        uuid: Uuid().v1(),
+        entry: unixEpoch,
+        description: 'test',
+      );
+
+      expect(
+        Task.fromJson(json.decode(json.encode(simpleTask.toJson()))),
+        simpleTask,
+      );
+    });
+
+    test('test parsing complex json task', () {
+      var complexTask = Task(
+        status: 'pending',
+        uuid: Uuid().v1(),
+        entry: unixEpoch,
+        description: 'test',
+        start: unixEpoch,
+        end: unixEpoch,
+        due: unixEpoch,
+        until: unixEpoch,
+        wait: unixEpoch,
+        modified: unixEpoch,
+        scheduled: unixEpoch,
+        recur: 'yearly',
+        mask: '--',
+        imask: Random().nextInt(pow(2, 32)),
+        parent: Uuid().v1(),
+        project: 'some_project',
+        priority: 'H',
+        depends: Uuid().v1(),
+        tags: '+some_tag',
+        annotations: [
+          Annotation(
+            entry: unixEpoch,
+            description: 'some annotation',
+          ),
+        ],
+      );
+
+      expect(
+        Task.fromJson(json.decode(json.encode(complexTask.toJson()))),
+        complexTask,
+      );
     });
   });
 }
