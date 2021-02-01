b0db16c6a2031e2a3537ee87eb99452ae15c4a7e
Brady Trainor <git_commit@tangential.info>
2021-02-01 00:00:00 +0000

    Show trace

--- task/lib/src/functions/show_exception_dialog.dart
+++ task/lib/src/functions/show_exception_dialog.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 void showExceptionDialog({context, e}) {
   print(e);
-  print('${e.runtimeType}');
+  print('${e.stackTrace}');
   showDialog(
     context: context,
     builder: (context) => AlertDialog(
       scrollable: true,
       title: SelectableText('${e.runtimeType}'),
-      content: SelectableText('$e'),
+      content: SelectableText('$e\n${e.stackTrace}'),
       actions: [
         ElevatedButton(
           child: Text('Ok'),
