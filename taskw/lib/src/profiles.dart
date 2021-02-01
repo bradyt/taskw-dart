3e4bf3128f04baa19aba98115ad0efd82b4ab52f
Brady Trainor <git_commit@tangential.info>
2021-02-01 00:00:00 +0000

    Add and apply more linting in taskw

--- taskw/lib/src/profiles.dart
+++ taskw/lib/src/profiles.dart
@@ -11,15 +11,14 @@ class Profiles {
       File('${base.path}/current-profile').writeAsStringSync(profile);
 
   List<String> listProfiles() {
-    var dir = Directory('${base.path}/profiles');
-    dir.createSync();
+    var dir = Directory('${base.path}/profiles')..createSync();
     return dir.listSync().map((entity) => entity.path.split('/').last).toList();
   }
 
   void addProfile() =>
       Directory('${base.path}/profiles/${Uuid().v1()}').createSync();
 
-  void deleteProfile(profile) {
+  void deleteProfile(String profile) {
     Directory('${base.path}/profiles/$profile').deleteSync(recursive: true);
     if (File('${base.path}/current-profile').existsSync()) {
       if (profile == File('${base.path}/current-profile').readAsStringSync()) {
--- taskw/lib/src/storage.dart
+++ taskw/lib/src/storage.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:uuid/uuid.dart';
+
 import 'package:taskc/taskc.dart';
 
 class Storage {
@@ -7,31 +9,32 @@ class Storage {
 
   Directory profile;
 
-  List<Task> listTasks() {
-    return [
-      for (var description in [
-        'foo',
-        'bar',
-        'baz',
-        'qux',
-        'quux',
-        'quuz',
-        'corge',
-        'grault',
-        'garply',
-        'waldo',
-        'fred',
-        'plugh',
-        'xyzzy',
-        'thud'
-      ])
-        Task(
-          description: description,
-        ),
-    ];
-  }
+  List<Task> listTasks() => [
+        for (var description in [
+          'foo',
+          'bar',
+          'baz',
+          'qux',
+          'quux',
+          'quuz',
+          'corge',
+          'grault',
+          'garply',
+          'waldo',
+          'fred',
+          'plugh',
+          'xyzzy',
+          'thud'
+        ])
+          Task(
+            status: 'pending',
+            uuid: Uuid().v1(),
+            entry: DateTime.now().toUtc(),
+            description: description,
+          ),
+      ];
 
   void addTask(Task task) {
-    print(task);
+    stdout.writeln(task);
   }
 }
