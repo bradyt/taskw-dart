6f7353a1e7dd64a606d7f7900f9deafaf2e66e93
Brady Trainor <git_commit@tangential.info>
2021-02-01 00:00:00 +0000

    Do not fetch fonts in runtime

--- task/lib/main.dart
+++ task/lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:task/task.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(TaskApp());
 }
 
