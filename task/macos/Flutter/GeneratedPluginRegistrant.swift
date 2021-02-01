c9321ecd3d8cfb90e66e3295b4aa3b17c2fc17bb
Brady Trainor <git_commit@tangential.info>
2021-02-01 00:00:00 +0000

    Update macos files

--- task/macos/Flutter/GeneratedPluginRegistrant.swift
+++ task/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker_writable
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerWritablePlugin.register(with: registry.registrar(forPlugin: "FilePickerWritablePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
