3b51a0beecb7179fa6e5a8a6a6bfe439aa15ad59
Brady Trainor <git_commit@tangential.info>
2021-02-01 00:00:00 +0000

    Run dart create --template package-simple taskw

---
+++ taskw/example/taskw_example.dart
@@ -0,0 +1,6 @@
+import 'package:taskw/taskw.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ taskw/lib/taskw.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library taskw;
+
+export 'src/taskw_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
