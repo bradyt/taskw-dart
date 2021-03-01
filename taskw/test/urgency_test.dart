3f260945eed9e3c7e0f42abe2e613fa0c0c574d9
Brady Trainor <git_commit@tangential.info>
2021-03-01 00:00:00 +0000

    Apply lints

--- taskw/test/urgency_test.dart
+++ taskw/test/urgency_test.dart
@@ -18,11 +18,11 @@ void main() {
   }
 
   test('test urgency', () {
-      expect(urgency(createTask(tags: ['next'])), 15.8);
-      expect(urgency(createTask(priority: 'H')), 6);
-      expect(urgency(createTask(priority: 'M')), 3.9);
-      expect(urgency(createTask(priority: 'L')), 1.8);
-      expect(urgency(createTask(tags: ['a', 'b', 'c'])), 1);
-      expect(urgency(createTask(due: DateTime.now())), 8.796);
+    expect(urgency(createTask(tags: ['next'])), 15.8);
+    expect(urgency(createTask(priority: 'H')), 6);
+    expect(urgency(createTask(priority: 'M')), 3.9);
+    expect(urgency(createTask(priority: 'L')), 1.8);
+    expect(urgency(createTask(tags: ['a', 'b', 'c'])), 1);
+    expect(urgency(createTask(due: DateTime.now())), 8.796);
   });
 }
