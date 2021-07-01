81124c13b35479b831c35b9b0afa7af4be9e88b3
Brady Trainor <git_commit@tangential.info>
2021-07-01 00:00:00 +0000

    Factor out ProfilesColumn

--- task/lib/src/routes/task_list_route.dart
+++ task/lib/src/routes/task_list_route.dart
@@ -39,115 +39,7 @@ class TaskListRoute extends StatelessWidget {
       ),
       drawer: Drawer(
         child: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  key: PageStorageKey('task-list'),
-                  children: [
-                    ListTile(
-                      title: Text('Profiles'),
-                      trailing: IconButton(
-                          icon: Icon(Icons.add),
-                          onPressed: () => profilesWidget.addProfile()),
-                    ),
-                    for (var entry in profilesMap.entries)
-                      ExpansionTile(
-                        key: PageStorageKey<String>('exp-${entry.key}'),
-                        leading: Radio<String>(
-                          value: entry.key,
-                          groupValue: currentProfile,
-                          onChanged: (profile) =>
-                              profilesWidget.selectProfile(profile!),
-                        ),
-                        title: SingleChildScrollView(
-                          key: PageStorageKey<String>('scroll-${entry.key}'),
-                          scrollDirection: Axis.horizontal,
-                          child: Text(
-                            entry.value ?? entry.key,
-                            style: GoogleFonts.firaMono(),
-                          ),
-                        ),
-                        children: [
-                          ListTile(
-                            leading: Padding(
-                              padding: EdgeInsets.all(12),
-                              child: Icon(Icons.edit),
-                            ),
-                            title: Text('Rename profile'),
-                            onTap: () => showDialog(
-                              context: context,
-                              builder: (context) => RenameProfileDialog(
-                                profile: entry.key,
-                                alias: entry.value,
-                                context: context,
-                              ),
-                            ),
-                          ),
-                          ListTile(
-                            leading: Padding(
-                              padding: EdgeInsets.all(12),
-                              child: Icon(Icons.link),
-                            ),
-                            title: Text('Configure Taskserver'),
-                            onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ConfigureTaskserverRoute(
-                                  ProfilesWidget.of(context).getStorage(
-                                    entry.key,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          ListTile(
-                              leading: Padding(
-                                padding: EdgeInsets.all(12),
-                                child: Icon(Icons.file_download),
-                              ),
-                              title: Text('Export tasks'),
-                              onTap: () {
-                                var tasks = ProfilesWidget.of(context)
-                                    .getStorage(entry.key)
-                                    .export();
-                                var now = DateTime.now()
-                                    .toIso8601String()
-                                    .replaceAll(RegExp(r'[-:]'), '')
-                                    .replaceAll(RegExp(r'\..*'), '');
-                                exportTasks(
-                                  contents: tasks,
-                                  suggestedName: 'tasks-$now.txt',
-                                );
-                              }),
-                          ListTile(
-                            leading: Padding(
-                              padding: EdgeInsets.all(12),
-                              child: Icon(Icons.delete),
-                            ),
-                            title: Text('Delete profile'),
-                            onTap: () => showDialog(
-                              context: context,
-                              builder: (context) => DeleteProfileDialog(
-                                profile: entry.key,
-                                context: context,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                  ],
-                ),
-              ),
-              if (Platform.isAndroid) ...[
-                Divider(),
-                ListTile(
-                  title: Text('Privacy policy:'),
-                  subtitle: Text('This app does not collect data.'),
-                ),
-              ],
-            ],
-          ),
+          child: ProfilesColumn(),
         ),
       ),
       body: Column(
@@ -255,6 +147,126 @@ class TaskListRoute extends StatelessWidget {
         tooltip: 'Add task',
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class ProfilesColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var profilesWidget = ProfilesWidget.of(context);
+
+    var profilesMap = profilesWidget.profilesMap;
+    var currentProfile = profilesWidget.currentProfile;
+
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            key: PageStorageKey('task-list'),
+            children: [
+              ListTile(
+                title: Text('Profiles'),
+                trailing: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () => profilesWidget.addProfile()),
+              ),
+              for (var entry in profilesMap.entries)
+                ExpansionTile(
+                  key: PageStorageKey<String>('exp-${entry.key}'),
+                  leading: Radio<String>(
+                    value: entry.key,
+                    groupValue: currentProfile,
+                    onChanged: (profile) =>
+                        profilesWidget.selectProfile(profile!),
+                  ),
+                  title: SingleChildScrollView(
+                    key: PageStorageKey<String>('scroll-${entry.key}'),
+                    scrollDirection: Axis.horizontal,
+                    child: Text(
+                      entry.value ?? entry.key,
+                      style: GoogleFonts.firaMono(),
+                    ),
+                  ),
+                  children: [
+                    ListTile(
+                      leading: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Icon(Icons.edit),
+                      ),
+                      title: Text('Rename profile'),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (context) => RenameProfileDialog(
+                          profile: entry.key,
+                          alias: entry.value,
+                          context: context,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Icon(Icons.link),
+                      ),
+                      title: Text('Configure Taskserver'),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ConfigureTaskserverRoute(
+                            ProfilesWidget.of(context).getStorage(
+                              entry.key,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                        leading: Padding(
+                          padding: EdgeInsets.all(12),
+                          child: Icon(Icons.file_download),
+                        ),
+                        title: Text('Export tasks'),
+                        onTap: () {
+                          var tasks = ProfilesWidget.of(context)
+                              .getStorage(entry.key)
+                              .export();
+                          var now = DateTime.now()
+                              .toIso8601String()
+                              .replaceAll(RegExp(r'[-:]'), '')
+                              .replaceAll(RegExp(r'\..*'), '');
+                          exportTasks(
+                            contents: tasks,
+                            suggestedName: 'tasks-$now.txt',
+                          );
+                        }),
+                    ListTile(
+                      leading: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Icon(Icons.delete),
+                      ),
+                      title: Text('Delete profile'),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (context) => DeleteProfileDialog(
+                          profile: entry.key,
+                          context: context,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ),
+        if (Platform.isAndroid) ...[
+          Divider(),
+          ListTile(
+            title: Text('Privacy policy:'),
+            subtitle: Text('This app does not collect data.'),
+          ),
+        ],
+      ],
     );
   }
 }
