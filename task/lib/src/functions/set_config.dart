4ffb1276dbb6b0d7311e6256128390c45c3b35e2
Brady Trainor <git_commit@tangential.info>
2021-11-01 00:00:00 +0000

    Restructure setConfig method

--- task/lib/src/functions/set_config.dart
+++ task/lib/src/functions/set_config.dart
@@ -6,25 +6,26 @@ import 'package:file_selector/file_selector.dart';
 import 'package:taskc/storage.dart';
 
 Future<void> setConfig({required Storage storage, required String key}) async {
+  String? contents;
+  String? name;
   if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
     var typeGroup = XTypeGroup(label: key, extensions: []);
     var file = await openFile(acceptedTypeGroups: [typeGroup]);
     if (file != null) {
-      var contents = await file.readAsString();
-      storage.home.addPemFile(
-        key: key,
-        contents: contents,
-        name: file.name,
-      );
+      contents = await file.readAsString();
+      name = file.name;
     }
   } else {
     await FilePickerWritable().openFile((fileInfo, file) async {
-      var contents = file.readAsStringSync();
-      storage.home.addPemFile(
-        key: key,
-        contents: contents,
-        name: fileInfo.fileName ?? Uri.parse(fileInfo.uri).pathSegments.last,
-      );
+      contents = file.readAsStringSync();
+      name = fileInfo.fileName ?? Uri.parse(fileInfo.uri).pathSegments.last;
     });
+  }
+  if (contents != null) {
+    storage.home.addPemFile(
+      key: key,
+      contents: contents!,
+      name: name,
+    );
   }
 }
