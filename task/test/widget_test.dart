f5d9ab2be5f1c94a5242279070914e59f6f52fd0
Brady Trainor <git_commit@tangential.info>
2022-03-01 00:00:00 +0000

    Add test of task_list_tile.dart for 100% line coverage

--- task/test/widget_test.dart
+++ task/test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: avoid_types_on_closure_parameters
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:io';
@@ -14,11 +15,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:uuid/uuid.dart';
 
+import 'package:taskj/json.dart';
+
 import 'package:task/main.dart';
 import 'package:task/task.dart';
 
 void main() {
-  // ignore: avoid_types_on_closure_parameters
   testWidgets('simple home page test', (WidgetTester tester) async {
     var testingDirectory = Directory('test/profiles/${const Uuid().v1()}')
       ..createSync(recursive: true);
@@ -30,5 +32,22 @@ void main() {
     );
 
     expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+  testWidgets('test task list item', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: TaskListItem(
+            Task(
+              (b) => b
+                ..status = 'pending'
+                ..uuid = const Uuid().v1()
+                ..entry = DateTime.now()
+                ..description = 'foo',
+            ),
+          ),
+        ),
+      ),
+    );
   });
 }
