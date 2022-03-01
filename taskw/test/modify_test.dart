45426fb059da49e74a9a45d09c4fdb3532b524c5
Brady Trainor <git_commit@tangential.info>
2022-03-01 00:00:00 +0000

    Test display of edits

--- taskw/test/modify_test.dart
+++ taskw/test/modify_test.dart
@@ -24,7 +24,7 @@ void main() {
           ..description = 'foo'
           ..entry = now,
       ));
-    Modify(
+    var draft = Modify(
       getTask: storage.data.getTask,
       mergeTask: storage.data.mergeTask,
       uuid: uuid,
@@ -38,8 +38,49 @@ void main() {
       ..setWait(now)
       ..setUntil(now)
       ..setPriority('H')
-      ..setTags(ListBuilder(['baz']))
-      ..changes
-      ..save(modified: () => now);
+      ..setTags(ListBuilder(['baz']));
+    var expected = 'description:\n'
+        '  old: foo\n'
+        '  new: bar\n'
+        'status:\n'
+        '  old: pending\n'
+        '  new: waiting\n'
+        'end:\n'
+        '  old: null\n'
+        '  new: ${(draft.changes['end']!['new'] as DateTime).toLocal()}\n'
+        'due:\n'
+        '  old: null\n'
+        '  new: ${now.toLocal()}\n'
+        'wait:\n'
+        '  old: null\n'
+        '  new: ${now.toLocal()}\n'
+        'until:\n'
+        '  old: null\n'
+        '  new: ${now.toLocal()}\n'
+        'priority:\n'
+        '  old: null\n'
+        '  new: H\n'
+        'tags:\n'
+        '  old: null\n'
+        '  new: [baz]';
+    expect(
+      draft.changes.entries.map((entry) {
+        var _old = entry.value['old'];
+        var _new = entry.value['new'];
+        if (_old is DateTime) {
+          _old = _old.toLocal();
+        }
+        if (_new is DateTime) {
+          _new = _new.toLocal();
+        }
+        return '${entry.key}:\n'
+            '  old: $_old\n'
+            '  new: $_new';
+      }).join('\n'),
+      expected,
+    );
+
+    draft.save(modified: () => DateTime.now().toUtc());
+    expect(draft.changes.isEmpty, true);
   });
 }
