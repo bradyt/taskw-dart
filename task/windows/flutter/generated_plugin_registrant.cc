ad7100dab8d2628639f1331927f53f4890be5df2
Brady Trainor <git_commit@tangential.info>
2022-05-01 00:00:00 +0000

    Run flutter pub get in task/

--- task/windows/flutter/generated_plugin_registrant.cc
+++ task/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_selector_windows/file_selector_windows.h>
+#include <file_selector_windows/file_selector_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FileSelectorPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
